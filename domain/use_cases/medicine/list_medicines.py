889e368d7b621f9256a0d75d7b7a73838cd7fa67
Filipe Synthis <goulartfs@gmail.com>
2023-05-24 17:48:01 -0300

    add case: raise exception when pharmacy not found and list empty medicines list

--- domain/use_cases/medicine/list_medicines.py
+++ domain/use_cases/medicine/list_medicines.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from domain.entities.medicine import Medicine
+from domain.use_cases.pharmacy.errors.pharmacy_not_found_error import  PharmacyNotFound
 from domain.repositories.pharmacy_repository import PharmacyRepository
 
 
@@ -10,4 +11,7 @@ class ListMedicines:
 
     def execute(self, pharmacy_id: str) -> List[Medicine]:
         stored_pharmacy = self.pharmacy_repository.find_by_id(pharmacy_id=pharmacy_id)
+        if not stored_pharmacy:
+            raise PharmacyNotFound
+
         return stored_pharmacy.medicines
