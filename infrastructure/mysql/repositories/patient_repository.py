6b6e98fda675fce29520acf41fecfe2fedcd1d1a
Filipe Synthis <goulartfs@gmail.com>
2024-04-03 17:16:19 -0300

    init mysql patient repository

--- infrastructure/mysql/repositories/patient_repository.py
+++ infrastructure/mysql/repositories/patient_repository.py
@@ -1,2 +1,6 @@
-class PatientRepository:
+from domain.repositories.patient_repository \
+    import PatientRepository as Repository
+
+# implementar o uso do banco Mysql (insert, delete, list)
+class PatientRepository(Repository):
     pass
